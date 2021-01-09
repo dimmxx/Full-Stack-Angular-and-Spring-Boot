78958779130fc7e98ca373f3b8b628c280267e61
dimmxx <dimmxx@gmail.com>
2021-01-09 17:36:42 +0200

    ecommerce-front

--- ecommerce-project/backend/spring-boot-ecommerce/src/main/java/com/example/springecommerce/repository/ProductRepository.java
+++ ecommerce-project/backend/spring-boot-ecommerce/src/main/java/com/example/springecommerce/repository/ProductRepository.java
@@ -4,8 +4,10 @@ import com.example.springecommerce.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Repository
 @RepositoryRestResource(path = "products")
+@CrossOrigin
 public interface ProductRepository extends JpaRepository <Product, Long>{
 }
